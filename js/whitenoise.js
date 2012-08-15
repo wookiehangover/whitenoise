f5b3b2ff548703a382c526ed054f98f25b0e5e47
Sam Breed <sam@quickleft.com>
2012-08-14 22:52:08 -0600

    adding fullscreen support

--- js/whitenoise.js
+++ js/whitenoise.js
@@ -1,3 +1,9 @@
 (function(){
-  document.body.className = Math.random() * 2 > 1 ? 'a' : 'b';
+  var b = document.body;
+  b.className = Math.random() * 2 > 1 ? 'a' : 'b';
+  b.addEventListener && b.addEventListener('click', function(e){
+    var req = Modernizr.prefixed('RequestFullScreen', b);
+    req && req();
+  }, true);
+
 })();
